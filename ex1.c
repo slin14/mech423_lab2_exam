4faa25be71d96031b7e576e43140f4dcc3b62b72
Sophie <sophielin14@gmail.com>
2023-10-14 10:13:25 -0700

    ex1

--- ex1.c
+++ ex1.c
@@ -3,10 +3,46 @@
 
 /**
  * main.c - ex1 for exam
+ *
+ * DCO (8Mz) -> SMCLK (8MHz/32 = 0.25MHz = 250kHz)
+ * output SMCLK on P3.4
  */
+
+// DCO (8 MHz) -> MCLK
+//             -> ACLK
+//             -> SMCLK
+void setup_clocks() {
+	CSCTL0  = CSKEY;            // password to access CS registers
+	CSCTL1 |= DCOFSEL_3;        // DCO 8 MHz
+	CSCTL2  = SELM__DCOCLK +    // DCO -> MCLK 
+		      SELA__DCOCLK +    // DCO -> ACLK 
+			  SELS__DCOCLK ;    // DCO -> SMCLK
+}
+
+// divide SMCLK by 32
+void div_32_SMCLK() {
+	CSCTL3 |= DIVS__32;
+}
+
+// output on P3
+void output_P3(int bit) {
+	P3DIR  |= bit;
+	P3SEL1 |= bit;
+	P3SEL0 |= bit;
+}
+
+/////////////////////////////////////////////////
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+
+	setup_clocks();
+	div_32_SMCLK();
+	output_P3(BIT4); // output SMCLK on P3.4
+
+	while(1) {
+		;
+	}
 	
 	return 0;
 }
